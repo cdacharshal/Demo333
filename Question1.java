c4ebfc690d8e958050be29aab0d548e74ebbc6d6
cdacharshal <114065084+cdacharshal@users.noreply.github.com>
2022-09-21 17:13:01 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,13 @@
+package assignment;
+
+//Q 1 Java program to print welcome message.
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		System.out.print("Welcome");
+	}
+
+}
